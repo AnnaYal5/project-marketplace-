51a4dabf3a2ee3adde3f4a0711500bc6d28cc8a8
kxneki <kirillbudko1972r@gmail.com>
2025-06-03 19:42:14 +0300

    user_reg+log with database + dotenv

--- app/models/models.py
+++ app/models/models.py
@@ -10,8 +10,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = 'postgresql://postgres:password@localhost/alembic_db'
+DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
